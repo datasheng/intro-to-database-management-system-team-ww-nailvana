5cdf3c955b242981389fb125413e42b12959237a
rockyrockz95 <jscott003@citymail.cuny.edu>
2024-04-25 23:58:22 -0400

    account form

--- Digital-Service/website/auth.py
+++ Digital-Service/website/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import *
+from .models import Provider, Customer
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import (
@@ -8,7 +8,7 @@ from flask_login import (
     logout_user,
     current_user,
 )
-from website.forms import SignUpForm, LoginForm
+from website.forms import SignUpForm, LoginForm, AccountForm
 
 # routes related to authorization
 auth = Blueprint("auth", __name__)
@@ -82,30 +82,36 @@ def sign_up():
         return redirect(url_for("views.home"))
     return render_template("signup.html", user=current_user, form=form)
 
+
+# current_user table
 def current_user_logged_in():
-    provider = Provider.query.filter_by(Email = current_user.Email).first()
-    customer = Customer.query.filter_by(Email = current_user.Email).first()
+    provider = Provider.query.filter_by(Email=current_user.Email).first()
+    customer = Customer.query.filter_by(Email=current_user.Email).first()
     if provider is not None:
-        print(current_user.Email) 
+        print(current_user.Email)
         return "provider"
-    elif customer is not None: 
+    elif customer is not None:
         print(current_user.Email)
         return "customer"
     else:
         return None
 
-@auth.route("/account", methods=["GET", "POST"])
-def account():
-    user_type = current_user_logged_in()
 
-    if current_user.is_authenticated:
-        if user_type == "provider":
-            provider = Provider.query.filter_by(Email = current_user.Email).first()
-            #return current_user.Email
-            return render_template("account.html", user=current_user, logged_in=provider)
-        if user_type == "customer":
-            customer = Customer.query.filter_by(Email = current_user.Email).first()
-            #return current_user.Email
-            return render_template("account.html", user=current_user, logged_in=customer)
-        else: 
-            return "Please log in to access this page", 401
+@auth.route("/account", methods=["GET", "POST"])
+@login_required
+def account():
+    form = AccountForm()
+    print(current_user.Password)
+
+    if form.validate_on_submit():
+        current_user.Username = form.username.data
+        current_user.Email = form.email.data
+        current_user.Password = generate_password_hash(
+            form.new_password.data, method="pbkdf2:sha256"
+        )
+
+        db.session.commit()
+        flash("Account updated", category="success")
+        return redirect(url_for("auth.account"))
+
+    return render_template("account.html", user=current_user, form=form)
